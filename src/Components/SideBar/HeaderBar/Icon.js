23a8d3d32977a6a55d907c711b9032c40d5aeb83
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-08 00:32:58 -0300

    feat: create icon component and add style

---
+++ src/Components/SideBar/HeaderBar/Icon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Icon = ({ iconName }) => {
+  return (
+    <div class="col s1 center">
+      <i class="Medium material-icons">{iconName}</i>
+    </div>
+  );
+};
+
+export default Icon;
--- src/Components/SideBar/HeaderBar/index.js
+++ src/Components/SideBar/HeaderBar/index.js
@@ -1,30 +1,23 @@
 import React from "react";
+import "./index.css";
+import Icon from "./Icon";
 
 const HeaderBar = () => {
-  const DEFAULT_ICONS = "Small material-icons";
+  const icons = ["data_usage", "message", "more_vert"];
+
   return (
-    <div class="row  blue-grey">
+    <div class="row  blue-grey HeaderBar" id="teste">
       <div class="col s9 ">
-        {" "}
         <img
           src="https://image.flaticon.com/icons/png/512/149/149071.png"
           alt="user"
           class="circle responsive-img"
           width={35}
-        />{" "}
+        />
       </div>
-      <div class="col s1 ">
-        {" "}
-        <i class={DEFAULT_ICONS}>data_usage</i>{" "}
-      </div>
-      <div class="col s1 ">
-        {" "}
-        <i class={DEFAULT_ICONS}>message </i>{" "}
-      </div>
-      <div class="col s1 ">
-        {" "}
-        <i class={DEFAULT_ICONS}>more_vert</i>{" "}
-      </div>
+      {icons.map((icon) => (
+        <Icon iconName={icon} />
+      ))}
     </div>
   );
 };
