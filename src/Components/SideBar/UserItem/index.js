9269feca9b76271566a30bfa129190d6c260f2eb
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-08 00:34:56 -0300

    refactor: remove unnecessary things and add style

--- src/Components/SideBar/UserItem/index.js
+++ src/Components/SideBar/UserItem/index.js
@@ -1,23 +1,35 @@
 import React from "react";
 import moment from "moment";
 import "moment/locale/pt-br";
+import "./index.css";
 
 moment.locale("pt-br");
 
 const UserItem = () => {
   return (
-    <div class="row">
-      <div class="col s12">
-        <div class="col s1"> user Img </div>
-        <div class="col s10">
-          <div class="row"> numero/nome </div>
-          <div class="row"> mensagem atual </div>
+    <div class="row user-item-container">
+      <div class="col s12 user-item">
+        <div class="col s1 center">
+          {" "}
+          <img
+            src="https://image.flaticon.com/icons/png/512/149/149071.png"
+            alt="user"
+            class="circle responsive-img"
+            width={35}
+          />
         </div>
-        <div class="col s1">
-          <div class="row">{moment().format("dddd")}</div>
+        <div class="col s10">
+          <p class="no-margin"> +55 21 969210913 </p>
+
+          <p class="message-text no-margin">
+            <i class="material-icons icon-message">done_all</i> mensagem atual{" "}
+          </p>
+        </div>
+        <div class="col s1 center">
+          <p class="no-margin message-text">{moment().format("dddd")}</p>
           <div class="row">
             {" "}
-            <i class="small material-icons">arrow_drop_down</i>{" "}
+            <i class="small material-icons">keyboard_arrow_down</i>{" "}
           </div>
         </div>
       </div>
